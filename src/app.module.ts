60b8ff5c58767449980760cafca3241258d57bdd
Devin <devdhera@gmail.com>
2019-05-30 20:58:02 +0530

    Configured MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { MaterialRequisitionNotesController } from './material-requisition-notes/material-requisition-notes.controller';
 import { MaterialRequisitionNotesService } from './material-requisition-notes/material-requisition-notes.service';
+import { MaterialRequisitionNotesModule } from './material-requisition-notes/material-requisition-notes.module';
+
+const config = require('./config/keys');
 
 @Module({
-  imports: [],
+  imports: [
+    MaterialRequisitionNotesModule,
+    MongooseModule.forRoot(config.mongoURI),
+  ],
   controllers: [AppController, MaterialRequisitionNotesController],
   providers: [AppService, MaterialRequisitionNotesService],
 })
-export class AppModule {
-}
+export class AppModule {}
