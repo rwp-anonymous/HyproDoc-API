d8c56f6b33ef3a98350242f5e6c2b9a039d2bf70
Devin <devdhera@gmail.com>
2019-05-30 22:09:43 +0530

    Prod Settings

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,23 @@
-import {NestFactory} from '@nestjs/core';
-import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
-import {AppModule} from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   /* Swagger Setup */
-  const options =
-      new DocumentBuilder()
-          .setTitle('HyproDoc - API')
-          .setDescription('REST client documentation for HyproDoc Platform')
-          .setVersion('1.0')
-          .addTag('hyprodoc')
-          .build();
+  const options = new DocumentBuilder()
+    .setTitle('HyproDoc - API')
+    .setDescription('REST client documentation for HyproDoc Platform')
+    .setVersion('1.0')
+    .addTag('hyprodoc')
+    .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
   /* End of Swagger Setup */
 
-  await app.listen(3000);
-} bootstrap();
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
